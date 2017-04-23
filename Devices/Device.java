a7007ef99aa0dcca4923493d1763918697b5536b
Hung-Ruey Chen <vbigmouse@gmail.com>
2017-04-23 02:22:16 -0400

    [Project 3][Devices] Complete Device.java

--- Devices/Device.java
+++ Devices/Device.java
@@ -16,148 +16,92 @@ import java.util.*;
 
 public class Device extends IflDevice
 {
-    /**
-        This constructor initializes a device with the provided parameters.
-	As a first statement it must have the following:
-
-	    super(id,numberOfBlocks);
-
-	@param numberOfBlocks -- number of blocks on device
-
-        @OSPProject Devices
-    */
     public Device(int id, int numberOfBlocks)
     {
         super(id,numberOfBlocks);
-        System.out.println("Device()");
         this.iorbQueue = new IOQueue();
     }
 
-    /**
-       This method is called once at the beginning of the
-       simulation. Can be used to initialize static variables.
-
-       @OSPProject Devices
-    */
     public static void init()
     {
-        // your code goes here
-        System.out.println("Device.init ");
-        
-        System.out.println("Device.init2" );
+
     }
 
-    /**
-       Enqueues the IORB to the IORB queue for this device
-       according to some kind of scheduling algorithm.
-       
-       This method must lock the page (which may trigger a page fault),
-       check the device's state and call startIO() if the 
-       device is idle, otherwise append the IORB to the IORB queue.
-
-       @return SUCCESS or FAILURE.
-       FAILURE is returned if the IORB wasn't enqueued 
-       (for instance, locking the page fails or thread is killed).
-       SUCCESS is returned if the IORB is fine and either the page was 
-       valid and device started on the IORB immediately or the IORB
-       was successfully enqueued (possibly after causing pagefault pagefault)
-       
-       @OSPProject Devices
-    */
     public int do_enqueueIORB(IORB iorb)
     {
 
+        System.out.println("iorb " + iorb.toString());
         iorb.getPage().lock(iorb);
         iorb.getOpenFile().incrementIORBCount();
         
-        System.out.println("iorb " + iorb.toString());
         Disk disk = (Disk)Device.get(iorb.getID());
-        
-        int block_no = iorb.getBlockNumber();
-        int bytes_per_block = (int)Math.pow(2,MMU.getVirtualAddressBits()-MMU.getPageAddressBits()); // bytes per block
-        System.out.println("bytes_per_block "+bytes_per_block+" getBytesPerSector "+disk.getBytesPerSector());
-        int sec_per_block = bytes_per_block / disk.getBytesPerSector();
-        System.out.println("block_per_sec "+sec_per_block+" trackpercylinder "+disk.getPlatters() + " getSectorsPerTrack "+disk.getSectorsPerTrack());
-        int cylinder = block_no * sec_per_block / disk.getSectorsPerTrack()/disk.getPlatters();
+        int cylinder = CalcuteCylinder(iorb);
         System.out.println("cylinder " + cylinder);
+
         iorb.setCylinder(cylinder);
-        IOQueue<Integer> qu=new IOQueue<Integer>();
-        System.out.println("isEmpty "+qu.isEmpty());
-        
         disk.getHeadPosition();   
-        qu.insert(4);
-        qu.insert(2);
-        qu.insert(3);
-        qu.insert(1);     
-        System.out.println("list " + qu.toString());
-        Collections.sort(qu.getSub(0,3));
-        System.out.println("list " + qu.toString());
+
         if (iorb.getThread().getStatus() == ThreadKill)
             return FAILURE;
         if (!disk.isBusy())
             disk.startIO(iorb);
         else
-            iorbQueue.isEmpty(); 
+        {
+            if(cylinder>disk.getHeadPosition()) // put in current scan
+            {
+                System.out.println("Disk busy, insert current scan queue. " + cylinder);
+                ((IOQueue)iorbQueue).addCurrentScan(iorb,cylinder);
+
+            } 
+            else // put in next scan
+            {
+                System.out.println("Disk busy, insert next scan queue. " + cylinder);    
+                ((IOQueue)iorbQueue).addNextScan(iorb,cylinder);
+            }
+        }
         return SUCCESS;
     }
 
-    /**
-       Selects an IORB (according to some scheduling strategy)
-       and dequeues it from the IORB queue.
 
-       @OSPProject Devices
-    */
     public IORB do_dequeueIORB()
     {
-        // your code goes here
-        return null;
+        if(((IOQueue)iorbQueue).length() == 0)
+            return null;
+        
+        if(((IOQueue)iorbQueue).current_ind>=((IOQueue)iorbQueue).length() ) // no current scan, go to head of queue
+            ((IOQueue)iorbQueue).current_ind = 0;
+
+        IORB next = ((IOQueue)iorbQueue).getIORB();
+        ((IOQueue)iorbQueue).delete(0);
+        return next;
     }
 
-    /**
-        Remove all IORBs that belong to the given ThreadCB from 
-	this device's IORB queue
-
-        The method is called when the thread dies and the I/O 
-        operations it requested are no longer necessary. The memory 
-        page used by the IORB must be unlocked and the IORB count for 
-	the IORB's file must be decremented.
-
-	@param thread thread whose I/O is being canceled
-
-        @OSPProject Devices
-    */
     public void do_cancelPendingIO(ThreadCB thread)
     {
-        // your code goes here
-
+        if(((IOQueue)iorbQueue).length() != 0)
+            ((IOQueue)iorbQueue).dequeue_thread(thread);
     }
 
-    /** Called by OSP after printing an error message. The student can
-	insert code here to print various tables and data structures
-	in their state just after the error happened.  The body can be
-	left empty, if this feature is not used.
-	
-	@OSPProject Devices
-     */
     public static void atError()
     {
         // your code goes here
 
     }
 
-    /** Called by OSP after printing a warning message. The student
-	can insert code here to print various tables and data
-	structures in their state just after the warning happened.
-	The body can be left empty, if this feature is not used.
-	
-	@OSPProject Devices
-     */
     public static void atWarning()
     {
         // your code goes here
 
     }
 
+    public static int CalcuteCylinder(IORB iorb)
+    {
+        Disk disk = (Disk)Device.get(iorb.getID());
+        int bytes_per_block = (int)Math.pow(2,MMU.getVirtualAddressBits()-MMU.getPageAddressBits()); // bytes per block
+        int sec_per_block = bytes_per_block / disk.getBytesPerSector();
+        return iorb.getBlockNumber() * sec_per_block / disk.getSectorsPerTrack()/disk.getPlatters();
+    }
+    
 
 }
 
--- Devices/IOQueue.java
+++ Devices/IOQueue.java
@@ -14,49 +14,162 @@ import osp.FileSys.*;
 import osp.Tasks.*;
 import java.util.*;
 
-public class IOQueue<Integer> implements GenericQueueInterface
+import javax.net.ssl.ExtendedSSLSession;
+
+
+public class IOQueue implements GenericQueueInterface
 {
-    //private LinkedList<E> list = new LinkedList<E>();
-    private List<Integer> list = new ArrayList<Integer>();
-/*    public IOQueue()
+
+    public class IORBNode implements Comparable<IORBNode>
     {
-        System.out.println("IOQueue.init");
-        this.list = new LinkedList<E>();
-    }*/
+        IORB iorb;
+        int cylinder;
+        public IORBNode(IORB io, int cy)
+        {
+            this.iorb=io;
+            this.cylinder=cy;
+        }
+        //@Override
+        public int compare(IORBNode N1, IORBNode N2)
+        {
+            return N1.cylinder-N2.cylinder;
+        } 
+        //@Override
+        public int compareTo(IORBNode N2)
+        {
+            return this.cylinder-N2.cylinder;
+        } 
+    }
+    //private LinkedList<E> list = new LinkedList<E>();
+    public List<IORBNode> cylinder_list = new ArrayList<IORBNode>();
+    public int current_ind = 0;
+    public int current_pos = 0;
     @Override
     public int length()
     { 
-        return list.size();
+        return cylinder_list.size();
     }
     @Override
     public boolean isEmpty()
     {
-        return list.isEmpty();
+        return cylinder_list.isEmpty();
     }
     @Override
     public boolean contains(Object obj)
     {
-        return list.contains(obj);
+        return cylinder_list.contains(obj);
     }
-    public void insert(Integer obj)
+    public void show()
     {
-        this.list.add(obj);
+        System.out.println("class show :"+this.getClass().toString());
     }
-    public void delete(Integer ind)
+    public void insert(IORB io, int cy)
     {
-        list.remove(ind);
+        IORBNode iorb_node = new IORBNode(io,cy);
+        this.cylinder_list.add(iorb_node);
+    }
+    public void insert(IORB io, int cy, int ind)
+    {
+        IORBNode iorb_node = new IORBNode(io,cy);
+        this.cylinder_list.add(ind,iorb_node);
+    }
+    public void delete(IORBNode iorb_node)
+    {
+        this.cylinder_list.remove(iorb_node);
+    }
+    public void delete(int ind)
+    {
+        this.cylinder_list.remove(ind);
     }
     public String toString()
     {
-        return list.toString();
+        return this.cylinder_list.toString();
     }
-    public Integer getValue(int ind)
+    public IORBNode getIORBNode()
     {
-        return list.get(ind);
+        return this.cylinder_list.get(this.current_ind);
     }
-    public List<Integer> getSub(int fIndex, int toIndex)
+    public IORBNode getIORBNode(int ind)
     {
-        return list.subList(fIndex, toIndex);
+        return this.cylinder_list.get(ind);
     }
-    
+    public IORB getIORB()
+    {
+        return this.cylinder_list.get(this.current_ind).iorb;
+    }
+    public IORB getIORB(int ind)
+    {
+        return (this.cylinder_list.get(ind)).iorb;
+    }
+    public Integer getCylinder(int ind)
+    {
+        return this.cylinder_list.get(ind).cylinder;
+    }
+    public List<IORBNode> getSub(int fIndex, int toIndex)
+    {
+        return this.cylinder_list.subList(fIndex, toIndex);
+    }
+
+    public void sortSub(int fIndex, int toIndex)
+    {
+        
+        Collections.sort(this.cylinder_list.subList(fIndex, toIndex));
+    }
+    public void addNextScan(IORB iorb, int cy)
+    {   
+        IORBNode obj = new IORBNode(iorb, cy);
+        for (IORBNode t:cylinder_list.subList(0, current_ind))
+        {
+            if(t.compareTo(obj)>0)
+            {
+                cylinder_list.add(cylinder_list.indexOf(t), obj);
+                current_ind= -1 * current_ind;
+                break;
+            }
+        }
+
+        if (current_ind<0)  //found suitable position
+            current_ind = -1 * current_ind + 1;
+        else    // no suitable position or empty next queue, add to current index
+        {
+            cylinder_list.add(current_ind, obj);
+            current_ind++;
+        }   
+    }
+    public void addCurrentScan(IORB iorb, int cy)
+    {
+        IORBNode obj = new IORBNode(iorb, cy);
+        for(IORBNode t:cylinder_list.subList(current_ind, cylinder_list.size()))
+        {
+            if(t.compareTo(obj)>0)
+            {
+                cylinder_list.add(cylinder_list.indexOf(t), obj);
+                current_ind= -1 * current_ind;
+                break;
+            }
+        }
+
+        if (current_ind<0)  // found suitable position
+            current_ind = -1 * current_ind;
+        else                // current queue empty or no suitable position. add to last.
+            cylinder_list.add(obj);
+    }
+    public void dequeue_thread(ThreadCB thread)
+    {
+        for(IORBNode t:cylinder_list)
+        {
+            if(t.iorb.getThread()==thread)
+            {
+                t.iorb.getPage().unlock();
+                t.iorb.getOpenFile().decrementIORBCount();
+                if(!t.iorb.getOpenFile().closePending)
+                    t.iorb.getOpenFile().closePending=true;
+                if(t.iorb.getOpenFile().getIORBCount()==0)
+                    t.iorb.getOpenFile().close();
+                
+                cylinder_list.remove(t);
+            }
+        }
+    }
 }
+    
